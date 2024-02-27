424022bea5d846452f5f2616591a85777e3abdbe
Carlos Sebastián Avila <savila.dimprenta@hcdn.gob.ar>
2024-02-27 12:02:52 -0300

    00_intro

---
+++ 00_intro/activity.js
@@ -0,0 +1,15 @@
+const nombre = "Sebastian";
+let edad = 43;
+const precio = 100;
+const nombredeSerie = "Prision Break";
+const peliculas = ["It", "superman", "pesadillas"];
+
+console.log(nombre);
+console.log(edad);
+console.log(precio);
+console.log(nombredeSerie);
+console.log(peliculas);
+
+edad++;
+
+console.log(edad);
