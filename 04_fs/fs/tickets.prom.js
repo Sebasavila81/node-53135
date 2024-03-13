7a0671c9fff79346742739ccd7e1a285af42f1a4
Carlos Sebastián Avila <savila.dimprenta@hcdn.gob.ar>
2024-03-13 08:27:01 -0300

    fs

---
+++ 04_fs/fs/tickets.prom.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const ruta = "./04_fs/tickets.json";
+const contenido = JSON.stringify([{ title: "hp1" }, { title: "hp2" }], null, 2);
+fs.promises
+  .writeFile(ruta, contenido)
+  .then((resultado) => console.log(resultado))
+  .catch((error) => console.log(error));
+
+fs.promises
+  .readFile(ruta, "utf-8")
+  .then((respuesta) => console.log(respuesta))
+  .catch((error) => console.log(error));
+
+fs.promises
+  .unlink(ruta)
+  .then(() => console.log("se ha eliminado"))
+  .catch((error) => console.log(error));
